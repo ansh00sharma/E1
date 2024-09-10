b0d8095fd41b42153497c50899aa83887ca3c0c1
ansh <sharma999ansh@gmail.com>
2024-09-10 20:30:35 +0530

    f6

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,9 @@
 pip install -r requirements.txt
 
-python3 manage.py collectstatic --noinput
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+python3.9 manage.py collectstatic --noinput
 
 mv static staticfiles_build
+
--- project/settings.py
+++ project/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['.vercel.app','localhost','.now.sh','127.0.0.1']
+# ALLOWED_HOSTS = ['.vercel.app','localhost','.now.sh','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 # HTTPS settings
 SESSION_COOKIE_SECURE= True
