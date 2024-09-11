92e6616b91d19ea767c5c646cb2168318195054c
ANSH SHARMA <75446872+ansh00sharma@users.noreply.github.com>
2024-09-11 13:18:27 +0530

    Update admin.py

--- menu/admin.py
+++ menu/admin.py
@@ -9,9 +9,12 @@ class CustomCategoryAdmin(admin.ModelAdmin):
     
 
 class CustomFoodItemAdmin(admin.ModelAdmin):
+    def category_name(self, obj):
+        return obj.category.category_name
+        
     prepopulated_fields = {'slug': ('food_title',)}
-    list_display = ('food_title','category__category_name','vendor','price','is_available','updated_at')
-    search_fields = ('food_title','category__category_name','vendor__vendor_name','price')
+    list_display = ('food_title','category_name','vendor','price','is_available','updated_at')
+    search_fields = ('food_title','category_name','vendor__vendor_name','price')
     list_filter = ('is_available',)
 
 admin.site.register(Category,CustomCategoryAdmin)
